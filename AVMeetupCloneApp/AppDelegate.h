08f6fa13ac0a15d5afcf197d691e241330694b1a
Ayuna NYC <ayunanyc@ayunas-macbook-pro.local>
2016-05-14 11:49:12 -0400

    Initial Commit

---
+++ AVMeetupCloneApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVMeetupCloneApp
+//
+//  Created by Ayuna NYC on 5/14/16.
+//  Copyright © 2016 Ayuna NYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AVMeetupCloneApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVMeetupCloneApp
+//
+//  Created by Ayuna NYC on 5/14/16.
+//  Copyright © 2016 Ayuna NYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
